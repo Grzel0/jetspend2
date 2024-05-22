cd47f447b7a7949a83ebaa2fd8cef6bc5c1bbbdb
Grzegorz <grzegorz.gejlo@cyfroweszkoly.pl>
2024-05-22 14:54:59 +0200

    przechowywanie i wyświetlanie

--- app/src/main/java/com/example/jetspend2/MainActivity.kt
+++ app/src/main/java/com/example/jetspend2/MainActivity.kt
@@ -6,16 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetspend2.ui.theme.Jetspend2Theme
 
 class MainActivity : ComponentActivity() {
@@ -33,16 +38,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun menedzerwydatkow(){
-    val wydatki = remember {
+    val wydatek = remember {
         mutableStateOf("")
     }
+    val wydatki = remember { mutableStateListOf<String>()}
     
-    Column {
-        OutlinedTextField(value = wydatki.value, onValueChange = {wydatki.value = it}, label = { Text(
+    Column(modifier = Modifier.padding(16.dp)) {
+        OutlinedTextField(value = wydatek.value, onValueChange = {wydatek.value = it}, label = { Text(
             text = "Wprowadź wydatek"
         )})
-        Button(onClick = { /*TODO*/ }) {
+        Button(onClick = { 
+        if (wydatek.value.isNotEmpty()){
+            wydatki.add(wydatek.value)
+            wydatek.value = ""
+        }
+        }) {
            Text(text = "Dodaj wydatek") 
+        }
+        LazyColumn {
+            items(wydatki) { wydatek -> Text(text = wydatek, modifier = Modifier.fillMaxWidth().padding(4.dp))
+            }
         }
     }
 }
