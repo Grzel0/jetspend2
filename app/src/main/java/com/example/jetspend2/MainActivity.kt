1fc88f8c4d6e88275f62c84d7946a31c786649e1
Grzegorz <grzegorz.gejlo@cyfroweszkoly.pl>
2024-05-22 17:13:30 +0200

    wybor kategorii

--- app/src/main/java/com/example/jetspend2/MainActivity.kt
+++ app/src/main/java/com/example/jetspend2/MainActivity.kt
@@ -5,19 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -42,11 +49,29 @@ fun menedzerwydatkow(){
         mutableStateOf("")
     }
     val wydatki = remember { mutableStateListOf<String>()}
+    val kategoria = listOf("Jedzenie", "Rozrywka", "Edukacja")
+    val wyborkategorii = remember{ mutableStateOf(kategoria[0])}
+    var expanded by remember { mutableStateOf(false)}
     
     Column(modifier = Modifier.padding(16.dp)) {
         OutlinedTextField(value = wydatek.value, onValueChange = {wydatek.value = it}, label = { Text(
-            text = "Wprowadź wydatek"
-        )})
+            text = "Wprowadź wydatek")}
+        )
+        Row {
+            TextField(value = wyborkategorii.value, onValueChange = {}, readOnly = true)
+            Button(onClick = {expanded = true}) {
+                Text(text = "Kategoria")
+            }
+            DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+                kategoria.forEach { category ->
+                    DropdownMenuItem(onClick = {
+                        wyborkategorii.value = category
+                        expanded = false
+                    }, text = { Text(category)})
+
+                }
+            }
+        }
         Button(onClick = { 
         if (wydatek.value.isNotEmpty()){
             wydatki.add(wydatek.value)
@@ -56,11 +81,15 @@ fun menedzerwydatkow(){
            Text(text = "Dodaj wydatek") 
         }
         LazyColumn {
-            items(wydatki) { wydatek -> Text(text = wydatek, modifier = Modifier.fillMaxWidth().padding(4.dp))
+            items(wydatki) { wydatek ->
+                Text(text = "$wydatek", modifier = Modifier
+                .fillMaxWidth().padding(4.dp))
             }
         }
     }
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
